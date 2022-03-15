11e4cdbfa13fc987b97dfadbea28e605b0114548
091910665 Freman <freman.nery01@gmail.com>
2022-03-14 20:24:31 -0600

    primer Screenshot del Proyecto GitMundo "Freman"

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Git Mundo</title>
+
+</head>
+<body>
+
+    <font size= '14' name='Temp Sans ITC'  >
+
+     <marquee> Saludos a Cordiales </marquee>   
+
+    </font>
+    
+</body>
+
+</html>
+
+
+
